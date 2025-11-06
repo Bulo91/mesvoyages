df6eba5cfb0ef3077912eaf5b3ca61c10ccaba8e
Bulo91 <bulentkurt802@gmail.com>
2025-11-06 03:16:04 +0100

    Phase 19 : gestion des mails

---
+++ src/Entity/Contact.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+/**
+ * Description of Contact
+ *
+ * @author kurtb
+ */
+class Contact {
+#[Assert\NotBlank()]
+#[Assert\Length(min:2, max:100)]
+private ?string $nom;
+
+#[Assert\NotBlank()]
+#[Assert\Email()]
+private ?string $email;
+
+#[Assert\NotBlank()]
+private ?string $message;
+
+function getNom(): ?string {
+    return $this->nom;
+}
+
+function getEmail(): ?string {
+    return $this->email;
+}
+
+function getMessage(): ?string {
+    return $this->message;
+}
+
+function setNom($nom): self {
+    $this->nom = $nom;
+    return $this;
+}
+
+function setEmail($email): self {
+    $this->email = $email;
+    return $this;
+}
+
+function setMessage($message): self {
+    $this->message = $message;
+    return $this;
+}
+
+
+}
